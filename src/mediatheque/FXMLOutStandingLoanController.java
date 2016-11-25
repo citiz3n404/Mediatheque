dc8b9ebab292436f732bc865cd06d849e5028433
Benjamin <bpe.krafft@gmail.com>
2016-11-25 19:56:53 +0100

    Update OutStandingLoan FXML + Controller

--- src/mediatheque/FXMLOutStandingLoanController.java
+++ src/mediatheque/FXMLOutStandingLoanController.java
@@ -1,32 +1,154 @@
 package mediatheque;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.Date;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.beans.property.ReadOnlyStringWrapper;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
  *
  * @author Benjamin
  */
+
 public class FXMLOutStandingLoanController extends ControlledScreen implements Initializable {
 
     @FXML
+    private Label tempUserLabel;
+    
+    @FXML
+    private Label tempMediasLabel;
+     
+    @FXML
     private ImageView iv_home;
+    
+    @FXML
+    private TableView<BorrowingCard> tableLoan;
+    
+    @FXML
+    private TableColumn<BorrowingCard, String> clientCol;
+    
+    @FXML
+    private TableColumn<BorrowingCard, String> mediaCol;
+
+    @FXML
+    private TableColumn<BorrowingCard, Date> reminderCol;
+
+    @FXML
+    private TableColumn<BorrowingCard, Boolean> overdatedCol;
+
+    @FXML
+    private TableColumn<BorrowingCard, Date> limitCol;
+    
+    @FXML
+    private TableView<BorrowingCard> tableUserLoan;
+    
+    @FXML
+    private TableColumn<BorrowingCard, String> titleCol;
+    
+    @FXML
+    private TableColumn<BorrowingCard, String> renduCol;
+    
+    @FXML
+    private TextField tf_search;
+    
+    @FXML
+    private Label firstNameLabel;
+    
+    @FXML
+    private Label lastNameLabel;
+    
+    @FXML
+    private ImageView profilPicture;
 
 
     /**
      * Initializes the controller class.
      */
+    
+    
+     @FXML
+    private void handleResearchAction(KeyEvent event){
+        String str = tf_search.getText();
+        ObservableList<BorrowingCard> tempList = FXCollections.observableArrayList();
+        
+        
+        //Load if function of str
+        if(!str.isEmpty()){
+            tableLoan.setItems(mediatheque.getLoansList());
+            for(int i=0; i<tableLoan.getItems().size(); i++){
+                if(tableLoan.getItems().get(i).getClient().getFirstName().toLowerCase().startsWith(str.toLowerCase()) || tableLoan.getItems().get(i).getMedia().getTitle().toLowerCase().startsWith(str.toLowerCase())){
+                    tempList.add(tableLoan.getItems().get(i));
+                }
+            }
+            tableLoan.setItems(tempList);
+        } else{
+            tableLoan.setItems(mediatheque.getLoansList());
+        }
+        
+        //System.out.println(str);
+    }
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         iv_home.setImage(new Image("file:img/home.png"));
+        
+        clientCol.setCellValueFactory(cellData -> new ReadOnlyStringWrapper(cellData.getValue().getClient().getFirstName()+" "+cellData.getValue().getClient().getLastName()));
+        mediaCol.setCellValueFactory(cellData -> new ReadOnlyStringWrapper(cellData.getValue().getMedia().getTitle()));
+        reminderCol.setCellValueFactory(new PropertyValueFactory<>("reminderDate"));
+        limitCol.setCellValueFactory(new PropertyValueFactory<>("limitDate"));
+        overdatedCol.setCellValueFactory(new PropertyValueFactory<>("overDated"));
+        titleCol.setCellValueFactory(cellData -> new ReadOnlyStringWrapper(cellData.getValue().getMedia().getTitle()));
+        renduCol.setCellValueFactory(cellData -> new ReadOnlyStringWrapper(cellData.getValue().getLimitDate().toString()));
+           
     }    
+    
+        @FXML
+    private void handleCartAction(ActionEvent event){
+        try {
+            Stage stage;
+            stage = new Stage();
+            //stage.setScene(new Scene(root));
+            stage.setTitle("My modal window");
+            stage.initModality(Modality.APPLICATION_MODAL);
+            
+            FXMLLoader myLoader = new FXMLLoader(getClass().getResource("FXMLNewLoan.fxml"));
+            Parent loadScreen = (Parent) myLoader.load();
+            ControlledScreen myScreenControler = ((ControlledScreen) myLoader.getController()); 
+            myScreenControler.setDatas(mediatheque);
+            myScreenControler.setScreenParent(sm);
+            myScreenControler.updateAfterLoadingScreen();
+            
+            stage.setScene(new Scene(loadScreen));
+            stage.showAndWait();
+        } catch (IOException ex) {
+            Logger.getLogger(FXMLHomeController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    
     
     @FXML
     protected void goToScreenHome(ActionEvent event){
@@ -36,11 +158,33 @@ public class FXMLOutStandingLoanController extends ControlledScreen implements I
 
     @Override
     public void updateAfterLoadingScreen() {
-        
+        tableLoan.setItems(mediatheque.getLoansList());
+        tableLoan.getSelectionModel().selectedItemProperty().addListener((observableValue, oldValue, newValue) -> {
+            //Check whether item is selected and set value of selected item to Label
+            if (tableLoan.getSelectionModel().getSelectedItem() != null) {
+                firstNameLabel.setText(tableLoan.getSelectionModel().getSelectedItem().getClient().getFirstName());
+                lastNameLabel.setText(tableLoan.getSelectionModel().getSelectedItem().getClient().getLastName());
+                profilPicture.setImage(new Image(tableLoan.getSelectionModel().getSelectedItem().getClient().getImg()));
+                
+                ObservableList<BorrowingCard> tempList = FXCollections.observableArrayList();
+                for(BorrowingCard bc : mediatheque.getLoansList()){
+                    if(bc.getClient() == tableLoan.getSelectionModel().getSelectedItem().getClient()){
+                        tempList.add(bc);
+                    }
+                }
+                tableUserLoan.setItems(tempList);
+                
+                
+                
+            }
+        });
     }
 
     @Override
     public void updateDatas() {
+        
+        tempUserLabel.setText(mediatheque.getTempCart().getClient().getFirstName()+" "+mediatheque.getTempCart().getClient().getLastName());
+        tempMediasLabel.setText(mediatheque.getTempCart().getMedias().size()+"");
         
     }
     
